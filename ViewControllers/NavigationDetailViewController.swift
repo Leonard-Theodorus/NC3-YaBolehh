3d4d805a3155970220d9257c9f2d48063ef95636
Leonard Theodorus <aoesotfs@gmail.com>
2023-07-21 23:31:34 +0700

    Fixed search bar and destination recommendation into sheets

---
+++ ViewControllers/NavigationDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationDetailViewController.swift
+//  CobainMaps
+//
+//  Created by Leee on 21/07/23.
+//
+
+import UIKit
+
+class NavigationDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+}
